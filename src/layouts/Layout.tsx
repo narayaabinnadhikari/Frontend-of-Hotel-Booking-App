8192a03df46feea1eb8be7e6b1dbe0972e026867
narayaabinnadhkari <narayanabin223@gmail.com>
2024-02-03 20:08:39 +0545

    adding layout and hero, header, footer components

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,24 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+// import SearchBar from "../components/SearchBar";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <Hero />
+      <div className="container mx-auto">
+        {/* <SearchBar /> */}
+      </div>
+      <div className="container mx-auto py-10 flex-1">{children}</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
